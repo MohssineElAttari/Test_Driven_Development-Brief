6eb1b6e5ab5a7861836b5dfe457ad19c89ddc107
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-17 18:24:05 +0100

    new update in client repository

--- src/main/java/com/tdd/project/Repository/ClientRipository.java
+++ src/main/java/com/tdd/project/Repository/ClientRipository.java
@@ -3,6 +3,10 @@ package com.tdd.project.Repository;
 import com.tdd.project.Entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClientRipository extends JpaRepository<Client,Long> {
-    
+    Client findByEmail(String email);
+    List<Client> findBySex(String sex);
+
 }

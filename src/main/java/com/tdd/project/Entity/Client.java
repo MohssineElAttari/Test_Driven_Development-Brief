507ff143b5187e41a694a2adc62bad7ae5a7aa63
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-17 15:46:30 +0100

    update entity Client

--- src/main/java/com/tdd/project/Entity/Client.java
+++ src/main/java/com/tdd/project/Entity/Client.java
@@ -8,6 +8,7 @@ import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
@@ -27,7 +28,9 @@ public class Client {
     @NonNull
     @NotBlank(message="Please enter your phone number")
     @Column(name = "number_phone")
-    private int phone;
+    @Pattern(regexp = "^(?:(?:(?:\\+|00)212[\\s]?(?:[\\s]?\\(0\\)[\\s]?)?)|0){1}(?:5[\\s.-]?[2-3]|(6|7)[\\s.-]?[13-9]){1}[0-9]{1}(?:[\\s.-]?\\d{2}){3}$", message = "{validation.client.phone.pattern}")
+    private String phone;
+
 
     @NonNull
     @NotBlank(message="Please enter your name")

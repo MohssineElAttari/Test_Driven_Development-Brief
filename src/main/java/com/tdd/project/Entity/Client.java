f94b8d4303f2426f1922da2cce796f617cab936f
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-26 20:20:57 +0100

    new update in class client

--- src/main/java/com/tdd/project/Entity/Client.java
+++ src/main/java/com/tdd/project/Entity/Client.java
@@ -6,6 +6,7 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 
@@ -17,30 +18,39 @@ import javax.validation.constraints.Pattern;
 @ToString
 @Builder
 public class Client {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NonNull
     @NotBlank(message="Please enter your email")
     @Email(message="Please respect the email format")
+    @Column(name = "EMAIL", unique = true)
     private String email;
 
     @NonNull
     @NotBlank(message="Please enter your number phone")
-    @Column(name = "number_phone")
+    @Column(name = "NUMBER_PHONE" )
     @Pattern(regexp = "(\\+212|1)(\\d){9}")
     private String phone;
 
     @NonNull
+    @Column(name = "NOM_COMPLETE")
     @NotBlank(message="Please enter your complete name")
     private String nomComplete;
 
     @NonNull
+    @Column(name = "AGE")
     private int age;
 
     @NonNull
+    @Enumerated(EnumType.STRING)
+    @Column(name="SEX")
     private SexEnumeration sex;
 
     @NonNull
+    @Column(name = "IS_ACTIVE")
     private boolean isActive;
+
 }

53c321d39d772567db757eed864035de11d36490
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-26 23:48:35 +0100

    add client ripository test

---
+++ src/test/java/com/tdd/project/Repository/ClientRipositoryTest.java
@@ -0,0 +1,36 @@
+package com.tdd.project.Repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientRipositoryTest {
+
+    @Test
+    void deleteInBatch() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findClientByEmail() {
+    }
+
+    @Test
+    void findClientBySex() {
+    }
+
+    @Test
+    void numberOfClinetBySex() {
+    }
+
+    @Test
+    void numberOfClientByAge() {
+    }
+
+    @Test
+    void clientActive() {
+    }
+}

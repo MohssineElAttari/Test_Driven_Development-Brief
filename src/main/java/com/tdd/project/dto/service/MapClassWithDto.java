44e781d0d201eb6d4ebeee3b3d4a2ca9485d670f
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-26 20:27:05 +0100

    class Map class with Dto

--- src/main/java/com/tdd/project/dto/service/MapClassWithDto.java
+++ src/main/java/com/tdd/project/dto/service/MapClassWithDto.java
@@ -1,15 +1,56 @@
 package com.tdd.project.dto.service;
 
-import javax.swing.text.html.parser.Entity;
+import lombok.AllArgsConstructor;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.modelmapper.convention.MatchingStrategies;
+
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface MapClassWithDto<ENTITYConvert,DTOConvert> {
+@Service
+public class MapClassWithDto<E, D> implements IMapClassWithDto<E,D> {
 
-    DTOConvert convertToDto(ENTITYConvert entityConvert, Class<DTOConvert> dtoClass);
-    ENTITYConvert ONVERT_ENTITY(DTOConvert dtoConvert,Class<ENTITYConvert>convertClass);
+    @Autowired
+    private ModelMapper modelMapper;
 
-    List<DTOConvert> convertListToListDto(Collection<ENTITYConvert> entityList, Class<DTOConvert> outCLass);
-    List<ENTITYConvert> convertListToListEntity(Collection<DTOConvert> dtoList, Class<ENTITYConvert> outCLass);
+
+    @Override
+    public D convertToDto(E entity, Class<D> dtoClass) {
+        if(entity == null){
+            return null;
+        }
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setPropertyCondition(Conditions.isNotNull());
+        return modelMapper.map(entity,dtoClass);
+    }
+
+    @Override
+    public E convertToEntity(D dto, Class<E> entityClass) {
+        if(dto == null)
+            return null;
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setPropertyCondition(Conditions.isNotNull());
+        return modelMapper.map(dto, entityClass);
+    }
+
+    @Override
+    public List<D> convertListToListDto(Collection<E> entityList, Class<D> outCLass) {
+        if(entityList == null)
+            return Collections.emptyList();
+
+        return entityList.stream().map(entity -> convertToDto(entity, outCLass)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<E> convertListToListEntity(Collection<D> dtoList, Class<E> outCLass) {
+        if(dtoList == null)
+            return Collections.emptyList();
+
+        return dtoList.stream().map(dto -> convertToEntity(dto, outCLass)).collect(Collectors.toList());
+    }
 
 }

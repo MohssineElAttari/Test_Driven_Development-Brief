c5b817bffcb14ec9d05abf5eeaa6a44f8fe212b0
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-27 22:57:16 +0000

    Add ClientControllerTest

---
+++ src/test/java/com/tdd/project/Controller/ClientControllerTest.java
@@ -0,0 +1,94 @@
+package com.tdd.project.Controller;
+
+import com.tdd.project.Entity.Client;
+import com.tdd.project.Enum.SexEnumeration;
+import com.tdd.project.Repository.ClientRipository;
+import com.tdd.project.Service.ClientService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebMvcTest(ClientController.class)
+class ClientControllerTest {
+
+    @MockBean
+    ClientService clientService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    Client client,client2,client3;
+    List<Client> clients;
+
+    @BeforeEach
+    void setUp(){
+        client=new Client(1L,"email1@gmail.com","+212123456789","mohssine",23, SexEnumeration.HOMME,true);
+        client2=new Client(2L,"email2@gmail.com","+212987654321","test",22, SexEnumeration.FAMME,true);
+        client3=new Client(3L,"email3@gmail.com","+212543216789","yap",21, SexEnumeration.HOMME,true);
+        clients =new ArrayList<>();
+        clients.add(client);
+        clients.add(client2);
+        clients.add(client3);
+    }
+    @Test
+    void addClient() {
+
+    }
+
+    @Test
+    void getAllClients() throws Exception {
+
+        BDDMockito.given(clientService.getAllClients()).willReturn(clients);
+//        Mockito.doReturn(clients).when(clientService).getAllClients();
+//        List<Client>clients1=clientService.getAllClients();
+        clients.forEach(c->{
+            System.out.println(c);
+        });
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/client/getAll")).andExpect(MockMvcResultMatchers.status().isOk()).
+                andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    void findByID() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findBySex() {
+    }
+
+    @Test
+    void updateClient() {
+    }
+
+    @Test
+    void deleteClient() {
+    }
+
+    @Test
+    void compteActive() {
+    }
+}

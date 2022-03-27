765e5c6e62a5f1fa23f3959a10e70eb6b95dab6b
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-27 16:21:24 +0000

    Client Ripository Test done ✌️✔️

--- src/test/java/com/tdd/project/Repository/ClientRipositoryTest.java
+++ src/test/java/com/tdd/project/Repository/ClientRipositoryTest.java
@@ -3,12 +3,14 @@ package com.tdd.project.Repository;
 import com.tdd.project.Entity.Client;
 import com.tdd.project.Enum.SexEnumeration;
 import com.tdd.project.Service.ClientService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -31,10 +33,14 @@ class ClientRipositoryTest {
 
     @Test
     void findByEmail() {
-        Mockito.lenient().when(clientRipository.findByEmail("Mohssine@gmail.com")).thenReturn(client1);
+//        Mockito.lenient().when(clientRipository.findByEmail("Mohssine@gmail.com")).thenReturn(client1);
+        Mockito.doReturn(client1).when(clientRipository).findByEmail("Mohssine@gmail.com");
+
         assertEquals(clientRipository.findByEmail("Mohssine@gmail.com"),client1);
         assertNotNull(clientRipository.findByEmail("Mohssine@gmail.com"));
         assertNull(clientRipository.findByEmail("test@gmail.com"));
+
+
     }
 
     @Test
@@ -50,7 +56,13 @@ class ClientRipositoryTest {
         clientList.add(client1);
         clientList.add(client2);
         clientList.add(client3);
+        Mockito.doReturn(clientList).when(clientRipository).findClientBySex(SexEnumeration.HOMME);
+//        assertFalse(clientRipository.findClientBySex(SexEnumeration.HOMME).isEmpty());
         Mockito.lenient().when(clientRipository.findClientBySex(SexEnumeration.HOMME)).thenReturn(clientList);
+        clientList.forEach(c->{
+            System.out.println(c.toString());
+        });
+
     }
 
     @Test
@@ -76,5 +88,9 @@ class ClientRipositoryTest {
 
     @Test
     void clientActive() {
+        Mockito.doReturn(clientList).when(clientRipository).ClientActive();
+        assertNotNull(clientRipository.ClientActive());
+        Mockito.verify(clientRipository,Mockito.times(1)).ClientActive();
+
     }
 }

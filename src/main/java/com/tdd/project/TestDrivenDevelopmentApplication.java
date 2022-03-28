8dd3710cfa8bcb0e6fc05ee41b29731931a78ffa
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-28 13:33:18 +0000

    new upadate

--- src/main/java/com/tdd/project/TestDrivenDevelopmentApplication.java
+++ src/main/java/com/tdd/project/TestDrivenDevelopmentApplication.java
@@ -1,9 +1,11 @@
 package com.tdd.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TestDrivenDevelopmentApplication {
 
     public static void main(String[] args) {
